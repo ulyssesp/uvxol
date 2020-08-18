1c155dfa73623157a4d6a96b595ee22feec1ca84
Ulysses Popple <ulysses.popple@gmail.com>
2020-08-18 13:12:39 +0000

    comments + local td instance

--- uvxol-web/src/store/modules/run.ts
+++ uvxol-web/src/store/modules/run.ts
@@ -21,22 +21,39 @@ import * as f from 'fp-ts/lib/Functor';
 import * as ot from 'fp-ts/lib/OptionT';
 import * as chn from 'fp-ts/lib/Chain';
 import { getFilterableComposition } from 'fp-ts/lib/Filterable';
+import rp from 'request-promise-native';
 
 const eqActionEvent = eq.eq.contramap(eq.eqNumber, (e: ActionEvent) => e.id);
 const seqT = array.array.sequence(task.task);
 const seqTSeq = array.array.sequence(task.taskSeq);
-const tseq: <A>(ta: task.Task<A>[]) => task.Task<void> = 
+const tseq: <A>(ta: task.Task<A>[]) => task.Task<void> =
   ta => fld.traverse_(task.taskSeq, array.array)(ta, task.map(constVoid))
-const tparallel: <A>(ta: task.Task<A>[]) => task.Task<void> = 
+const tparallel: <A>(ta: task.Task<A>[]) => task.Task<void> =
   ta => fld.traverse_(task.task, array.array)(ta, task.map(constVoid))
+const checkVoteOptions:
+  (chosenVoteOptions: { [id: number]: number }) =>
+    (target: boolean) =>
+      (checkVoteOptions: VoteOption[]) => boolean[] = chosenVoteOptions => target =>
+        array.map((voteOption: VoteOption) => (chosenVoteOptions[voteOption.id] === undefined) === target)
 
-{/* const taskOptionF = f.getFunctorComposition(task.task, option.option); */}
-{/* const taskOptionC = f.getFunctorComposition(task.task, option.option); */}
+const checkEventShouldRun = (chosenVoteOptions: { [id: number]: number }) => (e: ActionEvent) =>
+  pipe(
+    array.getMonoid<boolean>().concat(
+      // Check if chosenVoteOptions contains dependencies
+      checkVoteOptions(chosenVoteOptions)(false)(e.dependencies),
+      // Check if chosenVoteOptions doesn't contain preventions
+      checkVoteOptions(chosenVoteOptions)(true)(e.preventions)
+    ),
+    ds => semigroup.fold(semigroup.semigroupAll)(true, ds),
+    // Convert `false` to None
+    option.fromPredicate(identity),
+  )
+
 declare module 'fp-ts/lib/HKT' {
   interface URItoKind<A> {
     TaskOption: task.Task<option.Option<A>>
   }
-} 
+}
 const taskOption: mo.Monad1<'TaskOption'> = {
   URI: 'TaskOption',
   ...ot.getOptionM(task.task)
@@ -44,66 +61,99 @@ const taskOption: mo.Monad1<'TaskOption'> = {
 
 @Module({ dynamic: true, name: 'runStore', store })
 class Run extends VuexModule {
+
+  // Generate all tasks required to run these events.
   static runEvents: (self: Run) => (es: Set<ActionEvent>) => task.Task<void>[] = self =>
     flow(
+      // Convert set to array ordering by duration
       set.toArray(ord.ord.contramap(ord.ordNumber, e => e.duration)),
+      //  Run al lthe valid events
       array.filterMap(Run.runEvent(self)),
-   )
-
-  static runEvent: (self: Run) => (e: ActionEvent) => option.Option<task.Task<void>> = self =>
-    flow(
-      option.fromNullable,
-      option.chainFirst(e => pipe(
-        array.getMonoid<boolean>().concat(
-            array.map((d: VoteOption) => self.chosenVoteOptions[d.id] !== undefined)(e.dependencies),
-            array.map((d: VoteOption) => self.chosenVoteOptions[d.id] === undefined)(e.preventions),
-          ),
-        ds => semigroup.fold(semigroup.semigroupAll)(true, ds),
-        option.fromPredicate(identity),
-      )),
-      // () => option.none
-      option.map(e => tparallel([
-          () => eventStore.getEventsForTrigger(e.id).then(constVoid),
-          pipe(
-            task.fromIO(() => { self.runList.push(e); }), 
-            task.delay(e.delay || 0),
-            task.apSecond(
-              pipe(
-                e.actions,
-                array.map<ty.Action, task.Task<option.Option<void>>>(a => pipe(
-                  task.fromIO(() => self.socket.send(`["${a.location}", "${a.filePath === undefined ? "" : a.filePath}"]`)),
-                  task.map(() => a.id),
-                  id => task.ap(id)(task.of(id => option.fromNullable(self.pendingVoteOptions[id]))),
-                  ta => taskOption.chain(ta, flow(
-                    array.sort(ord.ordNumber),
-                    array.chop(as => {
-                      const { init, rest } = array.spanLeft((a: number) => eq.eqNumber.equals(a, as[0]))(as)
-                      return [init, rest];
-                    }),
-                    array.map(arr => [arr[0], arr.length] as [number, number]),
-                    nonEmptyArray.fromArray,
-                    option.map(nonEmptyArray.max(ord.ord.contramap(ord.ordNumber, e => e[1]))),
-                    option.map(tuple.fst),
-                    task.of
-                  )),
-                  t => taskOption.ap(taskOption.of(r => { Vue.set(self.chosenVoteOptions, r, r); }), t),
-                )),
-                tseq,
-                task.chain(() =>
-                  pipe(
-                    task.fromIO(() => option.fromNullable(eventStore.eventsByTrigger[e.id])),
-                    task.map(option.getOrElse(constant(set.empty as Set<number>))),
-                    task.map(set.filterMap(eqActionEvent)(id => option.fromNullable(eventStore.events[id]))),
-                    task.chain(flow(Run.runEvents(self), tseq)))),
-              task.delay(e.duration || 0))
-            )),
-      ])),
-      // option.map(tparallel),
     )
 
+  // Generate a task to run this event. This task includes data fetching, triggering other events, etc.
+  static runEvent: (self: Run) => (e: ActionEvent) => option.Option<task.Task<void>> = self =>
+    flow(
+      // Convert the (possibly null) ActionEvent to an Option
+      option.fromNullable,
+      // If the event shouldn't be run, return None
+      option.chainFirst(checkEventShouldRun(self.chosenVoteOptions)),
+      option.map(e => tparallel([
+        // Fetch events for the trigger id
+        () => eventStore.getEventsForTrigger(e.id).then(constVoid),
+        // Run the events
+        pipe(
+          // Push the event into the run list
+          task.fromIO(() => { self.runList.push(e); }),
+          // Wait out the delay
+          task.delay(e.delay || 0),
+          // Only return the delay action (?)
+          task.apSecond(
+            pipe(
+              e.actions,
+              array.map<ty.Action, task.Task<option.Option<void>>>(a => pipe(
+                // Send the location and filepath to the websocket
+                task.fromIO(() => Run.sendToTD(self, a.location, a.filePath)),
+                // map the result to the action id
+                task.map(() => a.id),
+                // Create a Task<Option<_>> from the pending vote options
+                id => task.ap(id)(task.of(id => option.fromNullable(self.pendingVoteOptions[id]))),
+                // Figure out the voteoptionid that won 
+                ta => taskOption.chain(ta, flow(
+                  // Sort the voteoptions
+                  array.sort(ord.ordNumber),
+                  // chop into same vote options
+                  array.chop(as => {
+                    const { init, rest } = array.spanLeft((a: number) => eq.eqNumber.equals(a, as[0]))(as)
+                    return [init, rest];
+                  }),
+                  // convert array of votes to [vote, count] tuple
+                  array.map(arr => [arr[0], arr.length] as [number, number]),
+                  // convert to nonemptyarray
+                  nonEmptyArray.fromArray,
+                  // grab the max based on the second tuple element
+                  option.map(nonEmptyArray.max(ord.ord.contramap(ord.ordNumber, e => e[1]))),
+                  // grabe the voteoption id of the tuple
+                  option.map(tuple.fst),
+                  // convert to task (?)
+                  task.of
+                )),
+                // Set chosenVoteOptions[winningVoteOptionId] to winningVoteOptionId
+                t => taskOption.ap(taskOption.of(r => { Vue.set(self.chosenVoteOptions, r, r); }), t),
+              )),
+              tseq,
+              task.chain(() =>
+                pipe(
+                  // Grab the triggered events
+                  task.fromIO(() => option.fromNullable(eventStore.eventsByTrigger[e.id])),
+                  // If there are none, create an empty set
+                  task.map(option.getOrElse(constant(set.empty as Set<number>))),
+                  // filter out any events we haven't fetched
+                  task.map(set.filterMap(eqActionEvent)(id => option.fromNullable(eventStore.events[id]))),
+                  // run the events in sequence
+                  task.chain(flow(Run.runEvents(self), tseq)))),
+              // Delay for duration
+              task.delay(e.duration || 0)
+            ))),
+      ])),
+    )
+
+  static sendToTD: (self: Run, location: string, filePath: string | undefined) => void =
+    (self, location, filePath) =>
+      rp.get({
+        url: "http://localhost:9980",
+        qs: {
+          location: location,
+          filePath: filePath
+        }
+      });
+
+  // self.socket.send(`["${action.location}", "${action.filePath === undefined ? "" : action.filePath}"]`))
+
+  // List of events that have run. Used for Debugging purposes.
   public runList: ActionEvent[] = [];
   public chosenVoteOptions: { [id: number]: number } = {};
-  public pendingVoteOptions: { [ id: number ] : Array<number> } = {};
+  public pendingVoteOptions: { [id: number]: Array<number> } = {};
   private socket = new WebSocket("ws://localhost:8080");
 
   get log() {
@@ -121,14 +171,17 @@ class Run extends VuexModule {
   @Mutation
   public async restart() {
     return pipe(
+      // Reset instance variables
       task.fromIO(() => {
         this.runList = [];
         this.chosenVoteOptions = {}
         this.pendingVoteOptions = {};
       }),
+      // fetch the start events
       task.chain(_ => constant(eventStore.getStartEvents())),
       task.chain(_ => task.of(eventStore.startEvents)),
       task.map(set.fromArray(eqActionEvent)),
+      // Run the start events in sequence
       task.chain(flow(Run.runEvents(this), array.array.sequence(task.task)))
     )();
   }
--- uvxol-web/src/views/Runner.vue
+++ uvxol-web/src/views/Runner.vue
@@ -1,68 +1,62 @@
 <template>
-    <v-container fluid grid-list-lg class="">
-      <v-row>
-        <h4> Status: {{ err }} </h4>
-      </v-row>
-      <v-row>
-        <v-btn @click="start()">Start</v-btn>
-      </v-row>
-      <v-row>
-        {{ chosenVoteOptions }}
-      </v-row>
-      <v-row class="flex-nowrap">
-        <v-col cols="2">
-          <v-row>
-            <v-subheader> Events </v-subheader>
-          </v-row>
-          <v-row v-for="(event, i) in events" :key="i" class="mb-3" cols = "2">
-            <Event v-bind:event="event" class="pa-1 flex-grow-1"></Event>
-          </v-row>
-        </v-col>
-        <v-col v-for="(location, i) in actionLogByLocation" :key="i" cols="2">
-          <v-row>
-            <v-subheader> {{ location[0] }} </v-subheader>
-          </v-row>
-          <v-row
-             v-for="(action, i) in location[1]"
-             :key="i"
-             class="mb-3"
-            >
-            <ActionC v-bind:action="action" class="ms-2 pa-1 flex-grow-1"></ActionC>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-container fluid grid-list-lg class>
+    <v-row>
+      <h4>Status: {{ err }}</h4>
+    </v-row>
+    <v-row>
+      <v-btn @click="start()">Start</v-btn>
+    </v-row>
+    <v-row>{{ chosenVoteOptions }}</v-row>
+    <v-row class="flex-nowrap">
+      <v-col cols="2">
+        <v-row>
+          <v-subheader>Events</v-subheader>
+        </v-row>
+        <v-row v-for="(event, i) in events" :key="i" class="mb-3" cols="2">
+          <Event v-bind:event="event" class="pa-1 flex-grow-1"></Event>
+        </v-row>
+      </v-col>
+      <v-col v-for="(location, i) in actionLogByLocation" :key="i" cols="2">
+        <v-row>
+          <v-subheader>{{ location[0] }}</v-subheader>
+        </v-row>
+        <v-row v-for="(action, i) in location[1]" :key="i" class="mb-3">
+          <ActionC v-bind:action="action" class="ms-2 pa-1 flex-grow-1"></ActionC>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
-import { ActionEvent, Action, VoteOption } from '../types'
-import { array, option, show } from 'fp-ts';
-import { pipe } from 'fp-ts/lib/pipeable';
-import { constant } from 'fp-ts/lib/function';
-import runStore from '../store/modules/run';
-import voteOptionStore from '../store/modules/voteoptions';
-import Event from '../components/Event.vue';
-import ActionC from '../components/Action.vue';
-import * as sg from 'fp-ts/lib/Semigroup';
-import * as fold from 'fp-ts/lib/Foldable';
-import * as m from 'fp-ts/lib/Monoid';
-import * as na from 'fp-ts/lib/NonEmptyArray';
-import * as r from 'fp-ts/lib/Record';
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { ActionEvent, Action, VoteOption } from "../types";
+import { array, option, show } from "fp-ts";
+import { pipe } from "fp-ts/lib/pipeable";
+import { constant } from "fp-ts/lib/function";
+import runStore from "../store/modules/run";
+import voteOptionStore from "../store/modules/voteoptions";
+import Event from "../components/Event.vue";
+import ActionC from "../components/Action.vue";
+import * as sg from "fp-ts/lib/Semigroup";
+import * as fold from "fp-ts/lib/Foldable";
+import * as m from "fp-ts/lib/Monoid";
+import * as na from "fp-ts/lib/NonEmptyArray";
+import * as r from "fp-ts/lib/Record";
 
-import { logid, logval } from '../utils/fp-utils';
+import { logid, logval } from "../utils/fp-utils";
 
 @Component({
-  components: { ActionC, Event }
+  components: { ActionC, Event },
 })
 export default class EventsList extends Vue {
-  private err = '';
+  private err = "";
   get actionLogByLocation() {
     return pipe(
       this.log,
-      array.chain(e => e.actions),
+      array.chain((e) => e.actions),
       na.fromArray,
-      option.map(na.groupBy(a => a.location.toLowerCase())),
+      option.map(na.groupBy((a) => a.location.toLowerCase())),
       option.map(r.toArray),
       option.getOrElse(constant([] as [string, na.NonEmptyArray<Action>][]))
     );
@@ -77,24 +71,22 @@ export default class EventsList extends Vue {
     return pipe(
       runStore.chosenVoteOptions,
       Object.values,
-      logval(array.array)(voteOptionStore.voteOptions),
-      array.map(k => voteOptionStore.voteOptions[k].name),
-      vos => fold.intercalate(m.monoidString, array.array)(', ', vos),
-      s => 'ChosenVoteOptions: ' + s
+      array.map((k) => voteOptionStore.voteOptions[k].name),
+      (vos) => fold.intercalate(m.monoidString, array.array)(", ", vos),
+      (s) => "ChosenVoteOptions: " + s
     );
   }
   private refresh() {
-    this.err = 'loading';
-    Promise.all([
-        runStore.start(),
-      ]).catch((e: any) => this.err = e)
-        .then(() => this.err = 'loaded');
+    this.err = "loading";
+    Promise.all([runStore.start()])
+      .catch((e: any) => (this.err = e))
+      .then(() => (this.err = "loaded"));
   }
   protected mounted() {
     this.refresh();
   }
   async start() {
-    runStore.start()
+    runStore.start();
   }
 }
 </script>
